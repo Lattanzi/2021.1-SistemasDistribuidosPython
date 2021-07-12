782b9d5c7b0544679e1754067e12085796359147
Lattanzi <lucas_latta@hotmail.com>
2021-07-12 11:41:50 -0300

    Update mysqlconn.py

--- environment/mysqlconn.py
+++ environment/mysqlconn.py
@@ -1,8 +1,8 @@
 import mysql.connector
 import json
 
-conn = mysql.connector.connect (host="sd2021-1.ch4slymbqpkt.us-east-1.rds.amazonaws.com", port="3306"
-, user="admin", passwd="admin2021", database="chinook")
+conn = mysql.connector.connect (host=" ", port=" "
+, user=" ", passwd=" ", database="chinook")
 
 cursor = conn.cursor()
 cursor.execute ("select * from genres")
