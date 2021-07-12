ce262cab285a7d231b51f80dbda30fba5de14473
Lattanzi <lucas_latta@hotmail.com>
2021-07-12 11:42:11 -0300

    Update myconn.py

--- environment/myconn.py
+++ environment/myconn.py
@@ -1,8 +1,8 @@
 import mysql.connector
 
 def conecta_chinook():
-    conn = mysql.connector.connect (host="sd2021-1.ch4slymbqpkt.us-east-1.rds.amazonaws.com", port="3306"
-    , user="admin", passwd="admin2021", database="chinook")
+    conn = mysql.connector.connect (host=" ", port=" "
+    , user=" ", passwd=" ", database="chinook")
     
     cursor = conn.cursor()
     
